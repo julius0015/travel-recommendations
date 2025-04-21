7110a66413e2c8581d990ea099362dd299efa019
Robot <juliusrabacajr0015@gmail.com>
2025-04-21 16:23:50 +0800

    Update travel_recommendation.js

--- travel/travel_recommendation.js
+++ travel/travel_recommendation.js
@@ -1,1 +1,9 @@
-
+fetch('./travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log("Travel Recommendation Data:", data);
+        // Further JavaScript logic for search and display will go here (Task 7 & 8)
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error);
+    });
